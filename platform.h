a408f2ac334f15fd78bcfbf6f5953f8a8e6653aa
Patrick Huesmann <info@patrick-huesmann.de>
2022-01-04 18:36:14 +0100

    Add support for STM32L433xx

--- platform.h
+++ platform.h
@@ -5,6 +5,8 @@
 /* Atmel SAM4L */
 #if defined(__ATSAM4LS8C__) || defined(__SAM4LS8C__)
 #include "sam4l.h"
+#elif defined(STM32L433xx)
+#include "stm32l433xx.h"
 #else
 #error "no platform defined"
 #endif
